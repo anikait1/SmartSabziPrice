177d52ba4cf2aeb49746597979bc7fd8c26b4595
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-06 16:13:05 +0530

    intial commit

---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Details extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Details Page</h1>
+            </div>
+        )
+    }
+}
