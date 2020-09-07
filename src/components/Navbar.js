2b0c82dd7ea93c235ee90389889a9968ce9b24ed
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-08 03:09:47 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,36 +3,71 @@ import React, { Component } from "react";
 export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <header>
-          <form className="form-inline">
-            <input type="search" placeholder="Location" aria-label="Search" />
-            <button type="submit">Search</button>
-          </form>
-          <nav>
-            <ul className="nav_links">
-              <li>
-                <a href="#">Vegetables</a>
-              </li>
-              <li>
-                <a href="#">Fruits</a>
-              </li>
-            </ul>
-          </nav>
-          <ul className="nav_links">
-            <li>
-              <a href="#">
-                <i className="fa fa-user-plus" /> Sign Up
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
+        <form className="form-inline">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Location"
+            />
+            <div className="input-group-append">
+              <button type="button" className="btn btn-secondary">
+                <i className="fa fa-search" />
+              </button>
+            </div>
+          </div>
+        </form>
+        <button
+          type="button"
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#navbarCollapse"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-between"
+          id="navbarCollapse"
+        >
+          <div className="navbar-nav">
+            <a href="#" className="nav-item nav-link">
+              Vegetables
+            </a>
+            <a href="#" className="nav-item nav-link">
+              Fruits
+            </a>
+            <div className="nav-item dropdown">
+              <a
+                href="#"
+                className="nav-link dropdown-toggle"
+                data-toggle="dropdown"
+              >
+                View By
               </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fa fa-user" /> Login
-              </a>
-            </li>
-          </ul>
-        </header>
-      </div>
+              <div className="dropdown-menu">
+                <a href="#" className="dropdown-item">
+                  Name
+                </a>
+                <a href="#" className="dropdown-item">
+                  Price
+                </a>
+                <a href="#" className="dropdown-item">
+                  Cateogory
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="navbar-nav">
+            <a href="#" className="nav-item nav-link">
+              <i className="fa fa-user-plus" /> SignUp
+            </a>
+            <a href="#" className="nav-item nav-link">
+              <i className="fa fa-user" /> Login
+            </a>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
