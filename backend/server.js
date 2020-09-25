a43c0fca501c87dae95980924aea00b10edb6b31
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-25 18:08:57 +0530

    added es6 syntax

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,13 @@
-const express  = require('express');
-const cors     = require('cors');
+import express from "express";
+import cors from "cors";
 
 const app = express();
-
+const json = express;
 app.use(cors());
-app.use(express.json());
 
 app.get("/", (req,res) => {
 
-    res.send("<h1>Hello Express File</h1>");
+    res.send("<h1>Hello Express</h1>");
 });
 
 //Giving Port No. 5000 for our local server to execute
