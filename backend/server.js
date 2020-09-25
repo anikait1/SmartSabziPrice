b2f471fe6d8e6b5f1e0c71fc6c68c6408b649972
Anikait <50035624+anikait1@users.noreply.github.com>
2020-09-26 03:55:39 +0530

    Added basic routes for posts entity

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
 
+import fruitRoutes from "./routes/fruits.js";
+import vegetableRoutes from "./routes/vegetables.js";
+
 const app = express();
-const json = express;
 app.use(cors());
 
+/*
 app.get("/", (req,res) => {
 
     res.send("<h1>Hello Express</h1>");
 });
+*/
+
+app.use("/fruits", fruitRoutes);
+app.use("/vegetables", vegetableRoutes);
 
 //Giving Port No. 5000 for our local server to execute
 app.listen("5000",() => {
