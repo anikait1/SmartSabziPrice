60f03394d66cd32a010725565843f2736322e463
Anikait <50035624+anikait1@users.noreply.github.com>
2020-09-26 14:34:38 +0530

    Update server.js
    Added the database connection

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,37 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config.js";
 
 import fruitRoutes from "./routes/fruits.js";
 import vegetableRoutes from "./routes/vegetables.js";
 
+// set up express
 const app = express();
+const port = process.env.PORT;
+
+// setup the middleware
 app.use(cors());
+app.use(express.json());
 
-/*
-app.get("/", (req,res) => {
-
-    res.send("<h1>Hello Express</h1>");
+// database connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
-*/
 
-app.use("/fruits", fruitRoutes);
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+// routes
 app.use("/vegetables", vegetableRoutes);
+app.use("/fruits", fruitRoutes);
 
-//Giving Port No. 5000 for our local server to execute
-app.listen("5000",() => {
-
-    console.log("Server Started on Port 5000");
+// Giving Port No. 5000 for our local server to execute
+app.listen(port, () => {
+  console.log("Server Started on Port 5000");
 });
