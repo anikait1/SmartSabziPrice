7a6e1825c7fd18f74a1c9bbf4c600b0f1846ccbd
Anikait <50035624+anikait1@users.noreply.github.com>
2020-09-26 14:34:27 +0530

    Prototyping route structure for posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,16 +3,18 @@ import express from "express";
 const router = express.Router();
 
 // GET all posts of a user
-router.get("/:userId", (req, res) => {
+router.get("/users/:userId", (req, res) => {
   res.send(`For userId: ${req.params.userId} sending all PricePosts`);
 });
 
 // GET posts based on an item ID
-router.get("/:itemId", (req, res) => {
+router.get("/items/:itemId", (req, res) => {
   const latitude = req.query.latitude;
   const longitude = req.query.longitude;
 
-  res.send(`Item ID: ${itemId}, Latitude: ${latitude}, Longitude: ${longitude}`);
+  res.send(
+    `Item ID: ${itemId}, Latitude: ${latitude}, Longitude: ${longitude}`
+  );
 });
 
 // GET posts for all fruits
@@ -21,7 +23,10 @@ router.get("/fruits", (req, res) => {
 });
 
 // GET posts for all vegetables
-router.get("/fruits", (req, res) => {
+router.get("/vegetables", (req, res) => {
   // discuss alternate for price display
 });
 export default router;
+
+// username: dbAdmin
+// password: lkYXgjB4kemszZU2
