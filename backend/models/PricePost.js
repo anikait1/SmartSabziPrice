e12d2d25c3b8eb3745f714da60c3f13a4b89aa00
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-27 14:19:12 +0530

    added timestamps

--- backend/models/PricePost.js
+++ backend/models/PricePost.js
@@ -1,52 +1,57 @@
 import { Schema, model } from "mongoose";
 
-const PricePostSchema = new Schema({
-  itemBill: {
-    price: {
-      type: Number,
-      required: true,
+const PricePostSchema = new Schema(
+  {
+    itemBill: {
+      price: {
+        type: Number,
+        required: true,
+      },
+
+      rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        required: false,
+      },
+
+      quantity: {
+        value: Number,
+        required: true,
+      },
+
+      quantityType: {
+        type: String,
+        enum: ["Dozen", "Gram", "Kilogram"],
+      },
     },
 
-    rating: {
+    votes: {
       type: Number,
       min: 0,
-      max: 5,
-      required: false,
     },
 
-    quantity: {
-      value: Number,
-      required: true,
+    // need to elaborate this
+    location: {
+      lattitude: String,
+      longitude: String,
+      pincode: String,
+      city: String,
+      state: String,
     },
 
-    quantityType: {
-      type: String,
-      enum: ["Dozen", "Gram", "Kilogram"],
+    author: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        //refernce via model User
+        ref: "User",
+      },
+      username: String,
     },
   },
-
-  votes: {
-    type: Number,
-    min: 0,
-  },
-
-  // need to elaborate this
-  location: {
-    lattitude: String,
-    longitude: String,
-    pincode: String,
-    city: String,
-    state: String,
-  },
-
-  author: {
-    id: {
-      type: mongoose.Schema.Types.ObjectId,
-      //refernce via model User
-      ref: "User",
-    },
-    username: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default model("PricePost", PricePostSchema);
