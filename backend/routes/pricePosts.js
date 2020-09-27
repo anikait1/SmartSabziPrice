6d3089785df55c247f0a661405c85a5e6cedd7d7
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-27 18:43:44 +0530

    updated pricePost

--- backend/routes/pricePosts.js
+++ backend/routes/pricePosts.js
@@ -1,9 +1,36 @@
 import express from "express";
+import PricePost from "../models/PricePost";
 
 const router = express.Router();
 
 // create a new PricePost Model
-router.post("/", (req, res) => {});
+router.post("/", (req, res) => {
+  
+    const itemBill = {
+    price: req.body.price,
+    rating: req.body.rating,
+    quantity: req.body.quantity,
+    quantityType: req.body.quantityType,
+  };
+  const votes = req.body.votes;
+  const location = req.body.location;
+
+  const author = {
+    id: req.user._id,
+    username: req.user.username,
+  };
+
+  const newPost = new PricePost({
+    itemBill,
+    votes,
+    location,
+    author,
+  });
+
+  newPost.save()
+    .then(() => res.json("New Post Added"))
+    .catch((err) => res.status(400).json("Error" + err));
+});
 
 // update vote count for a PricePost
 router.patch("/:postId", (req, res) => {});
