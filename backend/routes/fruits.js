4dce8360f192de3169cfd2f8e89d9ad91805ab6b
Anikait <50035624+anikait1@users.noreply.github.com>
2020-09-27 15:27:24 +0530

    Basic data fetching
    Added routes for basic data fetching

--- backend/routes/fruits.js
+++ backend/routes/fruits.js
@@ -1,19 +1,30 @@
 import express from "express";
+import Item from "../models/Item";
 
 const router = express.Router();
 
 // GET all fruits
-router.get("/", (req, res) => {
-  // using query parameterss
-  const name = req.query.name;
-  const age = req.query.age;
+router.get("/", async (req, res) => {
+  try {
+    const fruits = await Item.find({ category: "Fruit" }).select({
+      name: 1,
+      imageUrl: 1,
+      "nutrition.calories": 1,
+    });
 
-  res.send(`GET ALL fruits Q: ${name}, ${age}`);
+    res.json(fruits);
+  } catch (err) {
+    res.json({ messag: err });
+  }
+
+  res.send("GET /");
 });
 
 // GET a single fruit
 router.get("/:id", (req, res) => {
   const fruitId = req.params.id;
+  const longitude = req.query.longitude;
+  const latitde = req.query.latitde;
 
   res.send(`GET A fruit: ${fruitId}`);
 });
--- backend/routes/vegetables.js
+++ backend/routes/vegetables.js
@@ -1,19 +1,28 @@
 import express from "express";
+import Item from "../models/Item";
 
 const router = express.Router();
 
 // GET all vegetables
-router.get("/", (req, res) => {
-  // using query parameterss
-  const name = req.query.name;
-  const age = req.query.age;
+router.get("/", async (req, res) => {
+  try {
+    const vegetables = await Item.find({ category: "Vegetable" }).select({
+      name: 1,
+      imageUrl: 1,
+      "nutrition.calories": 1,
+    });
 
-  res.send(`GET ALL vegetables Q: ${name}, ${age}`);
+    res.json(vegetables);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 // GET a single vegetable
 router.get("/:id", (req, res) => {
   const vegetableId = req.params.id;
+  const longitude = req.query.longitude;
+  const latitde = req.query.latitde;
 
   res.send(`GET A vegetable: ${vegetableId}`);
 });
