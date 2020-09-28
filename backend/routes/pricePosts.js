a429d067208dcf0f99cc1e04041b111a7dceeb91
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-28 12:06:40 +0530

    Update pricePost.js

--- backend/routes/pricePosts.js
+++ backend/routes/pricePosts.js
@@ -4,8 +4,8 @@ import PricePost from "../models/PricePost";
 const router = express.Router();
 
 // create a new PricePost Model
-router.post("/vegetables/:id", (req, res) => {
-  Item.findById(req.params.id, (err, itemfound) => {
+router.post("/", (req, res) => {
+  Item.findById(req.body.id, (err, itemfound) => {
     if (err) {
       console.log(err);
     } else {
@@ -13,71 +13,11 @@ router.post("/vegetables/:id", (req, res) => {
         if (err) {
           console.log(err);
         } else {
-          const itemBill = {
-            price: req.body.price,
-            rating: req.body.rating,
-            quantity: req.body.quantity,
-            quantityType: req.body.quantityType,
-          };
+          const itemBill = req.body.itemBill;
           const votes = req.body.votes;
 
           // TO be modified acc. to current location
-          const location = {
-            lattitude: req.body.lattitude,
-            longitude: req.body.longitude,
-            pincode: req.body.pincode,
-            city: req.body.city,
-            state: req.body.state,
-          };
-          const author = {
-            id: req.user._id,
-            username: req.user.username,
-          };
-
-          newpricepost = new PricePost({
-            itemBill,
-            votes,
-            location,
-            author,
-          });
-
-          itemfound.priceposts.push(newpricepost);
-
-          newpricepost.save()
-            .then(() => res.json("New Post Added"))
-            .catch((err) => res.status(400).json("Error" + err));
-        }
-      });
-    }
-  });
-});
-
-router.post("/fruits/:id", (req, res) => {
-  Item.findById(req.params.id, (err, itemfound) => {
-    if (err) {
-      console.log(err);
-    } else {
-      PricePost.create(req.body.PricePost, (err, newpricepost) => {
-        if (err) {
-          console.log(err);
-        } else {
-          const itemBill = {
-            price: req.body.price,
-            rating: req.body.rating,
-            quantity: req.body.quantity,
-            quantityType: req.body.quantityType,
-          };
-          const votes = req.body.votes;
-
-          //TO be modified acc. to current location
-          const location = {
-            lattitude: req.body.lattitude,
-            longitude: req.body.longitude,
-            pincode: req.body.pincode,
-            city: req.body.city,
-            state: req.body.state,
-          };
-
+          const location = req.body.location;
           const author = {
             id: req.user._id,
             username: req.user.username,
