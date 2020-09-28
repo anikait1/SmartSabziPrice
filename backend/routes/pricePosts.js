9d45c848c77d1323bbe0dec1522c14ef30557dee
Sahil Rajput <sahilrajput85579@gmail.com>
2020-09-28 11:43:07 +0530

    updated picePost.js

--- backend/routes/pricePosts.js
+++ backend/routes/pricePosts.js
@@ -4,6 +4,7 @@ import PricePost from "../models/PricePost";
 const router = express.Router();
 
 // create a new PricePost Model
+<<<<<<< Updated upstream
 router.post("/", (req, res) => {
   const itemBill = {
     price: req.body.price,
@@ -24,12 +25,110 @@ router.post("/", (req, res) => {
     votes,
     location,
     author,
-  });
+=======
+router.post("/vegetables/:id", (req, res) => {
+  Item.findById(req.params.id, (err, itemfound) => {
+    if (err) {
+      console.log(err);
+    } else {
+      PricePost.create(req.body.PricePost, (err, newpricepost) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const itemBill = {
+            price: req.body.price,
+            rating: req.body.rating,
+            quantity: req.body.quantity,
+            quantityType: req.body.quantityType,
+          };
+          const votes = req.body.votes;
 
+          // TO be modified acc. to current location
+          const location = {
+            lattitude: req.body.lattitude,
+            longitude: req.body.longitude,
+            pincode: req.body.pincode,
+            city: req.body.city,
+            state: req.body.state,
+          };
+          const author = {
+            id: req.user._id,
+            username: req.user.username,
+          };
+
+          newpricepost = new PricePost({
+            itemBill,
+            votes,
+            location,
+            author,
+          });
+
+          itemfound.priceposts.push(newpricepost);
+
+          newpricepost.save()
+            .then(() => res.json("New Post Added"))
+            .catch((err) => res.status(400).json("Error" + err));
+        }
+      });
+    }
+>>>>>>> Stashed changes
+  });
+});
+
+router.post("/fruits/:id", (req, res) => {
+  Item.findById(req.params.id, (err, itemfound) => {
+    if (err) {
+      console.log(err);
+    } else {
+      PricePost.create(req.body.PricePost, (err, newpricepost) => {
+        if (err) {
+          console.log(err);
+        } else {
+          const itemBill = {
+            price: req.body.price,
+            rating: req.body.rating,
+            quantity: req.body.quantity,
+            quantityType: req.body.quantityType,
+          };
+          const votes = req.body.votes;
+
+<<<<<<< Updated upstream
   newPost
     .save()
     .then(() => res.json("New Post Added"))
     .catch((err) => res.status(400).json("Error" + err));
+=======
+          //TO be modified acc. to current location
+          const location = {
+            lattitude: req.body.lattitude,
+            longitude: req.body.longitude,
+            pincode: req.body.pincode,
+            city: req.body.city,
+            state: req.body.state,
+          };
+
+          const author = {
+            id: req.user._id,
+            username: req.user.username,
+          };
+
+          newpricepost = new PricePost({
+            itemBill,
+            votes,
+            location,
+            author,
+          });
+
+          itemfound.priceposts.push(newpricepost);
+
+          newpricepost.save()
+            .then(() => res.json("New Post Added"))
+            .catch((err) => res.status(400).json("Error" + err));
+        }
+      });
+    }
+  });
+>>>>>>> Stashed changes
 });
 
 // update vote count for a PricePost
