d7d9e2bdde25f7fe8cb4b8ad7e808e52bb04625a
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-03 00:34:22 +0530

    Update PricePost.js
    Formatted the code

--- backend/models/PricePost.js
+++ backend/models/PricePost.js
@@ -3,46 +3,23 @@ import mongoose from "mongoose";
 const PricePostSchema = new mongoose.Schema(
   {
     itemBill: {
-      price: {
-        type: Number,
-        required: true,
-      },
-      rating: {
-        type: Number,
-        min: 0,
-        max: 5,
-        required: false,
-      },
-      quantity: {
-        value: Number,
-        required: true,
-      },
-      quantityType: {
-        type: String,
-        enum: ["Dozen", "Gram", "Kilogram"],
-      },
+      price: { type: Number, required: true },
+      rating: { type: Number, min: 0, max: 5, required: false },
+      quantity: { value: Number, required: true },
+      quantityType: { type: String, enum: ["Dozen", "Gram", "Kilogram"] },
     },
 
-    votes: {
-      type: Number,
-      default: 0,
-    },
+    votes: { type: Number, default: 0 },
 
     location: {
       position: {
-        type: {
-          type: String,
-          enum: ["Point"],
-          required: true,
-        },
-        coordinates: {
-          type: [Number], // [longitude, latitude]
-          required: true,
-        },
+        // mongoose GeoJson
+        type: { type: String, enum: ["Point"], required: true },
+        coordinates: { type: [Number], required: true }, // [longitude, latitude]
       },
-      pincode: String,
-      city: String,
-      state: String,
+      pincode: { type: String, required: false },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
     },
 
     author: {
