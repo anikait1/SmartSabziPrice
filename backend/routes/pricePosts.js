0c14cc6dc7aca305232c19fbc3fbe95bfb276785
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-06 01:31:31 +0530

    tried diffrent syntax

--- backend/routes/pricePosts.js
+++ backend/routes/pricePosts.js
@@ -21,23 +21,44 @@ router.get("/users/:userId", (req, res) => {
 
 // get all posts for a specific item
 router.get("/items/:itemId", (req, res) => {
+  
   const longitude = req.query.longitude;
   const latitude = req.query.latitude;
 
-  PricePost.aggregate([
-    {
-      $geoNear: {
-        near: {
-          type: "Point",
-          coordianates: [longitude, latitude],
-        },
-        maxDistance: 1000,
-        distanceField: "location.position",
-      },
-    },
-  ])
-    .then((posts) => res.json(posts))
-    .catch((err) => res.json({ message: err }));
+ PricePost.find({
+    location : {
+      $near : {
+        $maxDistance : 10000,
+        $geometry : {
+          type : "Point",
+          coordianates : [longitude, latitude]
+        }
+      }
+    }
+  }).find((err,result) => {
+    if(err){
+      res.json({message : err});
+    console.log(err);
+    }else{
+      console.log(JSON.stringify(result,0,2));
+       res.json(result);
+    }
+  });
+
+  // PricePost.aggregate([
+  //   {
+  //     $geoNear: {
+  //       near: {
+  //         type: "Point",
+  //         coordianates: [longitude, latitude],
+  //       },
+  //       maxDistance: 1000,
+  //       distanceField: "location.position",
+  //     },
+  //   },
+  // ])
+  //   .then((posts) => res.json(posts))
+  //   .catch((err) => res.json({ message: err }));
 });
 
 router.get("/", (req, res) => {
