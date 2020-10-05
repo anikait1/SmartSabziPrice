6caa82178a13b4adb912b4266fdbce333512bfff
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-06 01:31:06 +0530

    changes in schema

--- backend/models/PricePost.js
+++ backend/models/PricePost.js
@@ -9,22 +9,27 @@ const PricePostSchema = new mongoose.Schema(
       quantityType: { type: String, enum: ["Dozen", "Gram", "Kilogram"] },
     },
     votes: { type: Number, default: 0 },
+    // location: {
+    //   position: {
+    //     // mongoose GeoJson
+    //     type: { type: String, enum: ["Point"], required: true },
+    //     coordinates: { type: [Number], required: true }, // [longitude, latitude]
+    //   },
+    //   pincode: { type: String, required: false },
+    //   city: { type: String, required: true },
+    //   state: { type: String, required: true },
+    // },
     location: {
-      position: {
-        // mongoose GeoJson
-        type: { type: String, enum: ["Point"], required: true },
-        coordinates: { type: [Number], required: true }, // [longitude, latitude]
-      },
-      pincode: { type: String, required: false },
-      city: { type: String, required: true },
-      state: { type: String, required: true },
-    },
-
+      type: { type: String },
+      coordinates: []
+     },
     // user and item details
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
     itemId: { type: mongoose.Schema.Types.ObjectId, ref: "Item" },
   },
   { timestamps: true }
 );
+
+PricePostSchema.index({ location: "2dsphere" });
 
 export default mongoose.model("PricePost", PricePostSchema);
