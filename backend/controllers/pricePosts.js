9f784d856bd1b5a59403f1e238620317a10e1d96
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-06 21:25:08 +0530

    Added groupby Query

--- backend/controllers/pricePosts.js
+++ backend/controllers/pricePosts.js
@@ -7,9 +7,48 @@ export const createPost = (req, res) => {
 };
 
 export const getUserPosts = (req, res) => {
-  PricePost.find({ userId: req.params.userId })
-    .then((posts) => res.status(200).json(posts))
-    .catch((err) => res.status(400).json({ message: err }));
+
+  // PricePost.find({ userId: req.params.userId })
+  //   .then((posts) => res.status(200).json(posts))
+  //   .catch((err) => res.status(400).json({ message: err }));
+
+  
+  // PricePost.find({ userId : req.params.userId},function(err, foundPost){
+  //   if(err){
+  //     res.json({message : err});
+  //     console.log(err);
+  //   }else{
+
+  //   }
+    
+  // });
+
+  PricePost.find({ userId: req.params.userId }).
+  then(
+
+      PricePost.aggregate([
+      {
+        "$group" : {
+            
+            // group by itemid or item
+            "_id" : '$item', 
+             postsByUser : {
+               $push : "$$ROOT"
+             }
+
+        }
+      }
+    ],function(err, result) {
+        if(err){
+          console.log(err);
+          res.json({message : err});
+        }else{
+          console.log(result);
+          res.json(result);
+        }
+    })
+  )
+  
 };
 
 export const getItemPosts = (req, res) => {
