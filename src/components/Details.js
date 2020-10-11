1279a0de412c40bce0d6ec4d5dc54dcdd6b450e8
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-11 14:45:40 +0530

    Broken into samlller Components

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PriceCardLayout from "./PriceCardLayout";
+import InputForm from './InputForm';
 
 const Details = () => {
   const imgLink =
@@ -22,57 +23,7 @@ const Details = () => {
             exercitationem sapiente eligendi iure debitis hic alias aliquam
             voluptatibus voluptatum recusandae earum?
           </p>
-          <form>
-            <div className="form-row no-gutters">
-              <div className="col-6">
-                <div className="input-group">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text">$</span>
-                  </div>
-                  <input
-                    type="number"
-                    class="form-control"
-                    placeholder="Price"
-                  ></input>
-                </div>
-              </div>
-              <div className="col-6">
-                <div className="input-group">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text">$</span>
-                  </div>
-                  <input
-                    type="number"
-                    class="form-control"
-                    placeholder="Ratings"
-                  ></input>
-                </div>
-              </div>
-              <div className="col-8 my-3">
-                <div className="input-group">
-                  <div className="input-group-prepend">
-                    <span className="input-group-text">$</span>
-                  </div>
-                  <input
-                    type="number"
-                    class="form-control"
-                    placeholder="Quantity"
-                  ></input>
-                </div>
-              </div>
-              <div className="col-4 my-3">
-                <select class="custom-select" id="inputGroupSelect02">
-                  <option selected>Units</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-              </div>
-              <div className="col-12 mb-3">
-                <button className="btn btn-primary btn-block">Confirm</button>
-              </div>
-            </div>
-          </form>
+          <InputForm />
         </div>
         <div className="col-12 col-lg-8">
           <PriceCardLayout />
