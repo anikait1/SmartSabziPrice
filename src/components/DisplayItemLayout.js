c4aa94fbb910a3ecaee45466be48feb3105ccda5
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-11 15:25:50 +0530

    Update DisplayItemLayout.js
    Added ajax call to the api

--- src/components/DisplayItemLayout.js
+++ src/components/DisplayItemLayout.js
@@ -1,34 +1,44 @@
 import DisplayItem from "./DisplayItem";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-const items = [
-  {
-    name: "Banana",
-    img: "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80",
-    price: 10,
-  },
-  {
-    name: "Apple",
-    img: "https://images.unsplash.com/photo-1579613832125-5d34a13ffe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjIxOTMzfQ&auto=format&fit=crop&w=1350&q=80",
-    price: 20,
-  },
-  {
-    name: "Pear",
-    img: "https://images.unsplash.com/photo-1540486117437-8946eede5364?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-    price: 50,
-  },
-];
+const DisplayItemLayout = ({ itemCategory }) => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
 
-const DisplayItemLayout = () => {
-  return (
-    <div className="row row-cols-2 row-cols-sm-4 row-cols-lg-6">
-      {items.map((item, key) => (
-        <div className="col mb-4">
-          <DisplayItem name={item.name} imgSrc={item.img} price={item.price} key={key} />
-        </div>
-      ))}
-    </div>
-  );
+  useEffect(() => {
+    fetch(`http://localhost:5000/items?category=${itemCategory}`)
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoaded(true);
+        setItems(result);
+      }, (error) => {
+        setIsLoaded(true);
+        setError(error);
+      })
+  }, [itemCategory]);
+
+  if (error) {
+    return <div className="text-danger">Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="row row-cols-2 row-cols-sm-4 row-cols-lg-6">
+        {items.map((item) => (
+          <div className="col mb-4" key={item._id}>
+            <DisplayItem
+              name={item.name}
+              imgSrc={item.displayImgUrl}
+              calorieCount={item.nutrition.calories}
+            />
+          </div>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default DisplayItemLayout;
