f5520303a7340be819c6ebe3be421c84fe5ba667
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-11 15:25:59 +0530

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,7 +5,7 @@ export default class HomePage extends Component {
   render() {
     return (
       <div className="container">
-        <DisplayItemLayout />
+        <DisplayItemLayout itemCategory="Fruit" />
       </div>
     );
   }
