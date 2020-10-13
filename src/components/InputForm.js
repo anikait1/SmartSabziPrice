f003f4c22f4e7b5e5fb62e3265f5138ac26eba1b
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-13 21:24:21 +0530

    Implemented with axios Post

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -82,11 +82,10 @@ export default class InputForm extends Component {
       userId: this.state.userId,
       itemId: this.state.itemId,
     };
-
     console.log(pricePost);
 
     axios
-      .post("http://localhost:5000/pricePosts", pricePost)
+      .post("http://localhost:5000/pricePosts", { pricePost })
       .then((res) => console.log(res.data))
       .catch((err) => console.log(err));
   }
@@ -164,129 +163,3 @@ export default class InputForm extends Component {
     );
   }
 }
-
-// import React from "react";
-// import axios from "axios";
-// import { useState } from "react";
-// import { useEffect } from "react";
-
-// const InputForm = (props) => {
-//   const [price, setPrice] = useState("");
-//   const [rating, setRating] = useState("");
-//   const [quantity, setQuantity] = useState("");
-//   const [quantityType, setQuantityType] = useState("");
-//   const [location, setLocation] = useState([]);
-//   const [error, setError] = useState(null);
-//   const [userId, setUserId] = useState('5f7b45788bbc5138dc7d9ab7');
-//   const [itemId, seeItemId] = useState('5f7b017009286604eee346a5');
-
-//   const submit = (event) => {
-//     event.preventDefault();
-
-//     if (!navigator.geolocation) {
-//       setError("Geolocation is not supported");
-//     } else {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           setLocation([position.coords.latitude, position.coords.longitude]);
-//         },
-//         () => {
-//           setError("Please Provide access to location for you Posting");
-//         }
-//       );
-//     }
-
-//     const pricePost = {
-
-//       itemBill : {
-//         price,
-//         rating,
-//         quantity,
-//         quantityType
-//       },
-//       location,
-//       userId,
-//       itemId
-//     };
-
-//     console.log(pricePost);
-
-//       axios
-//       .post("http://localhost:5000/pricePosts", pricePost)
-//       .then((res) => console.log(res.data))
-//       .catch((err) => console.log(err));
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={submit}>
-//         <div className="form-row no-gutters">
-//           <div className="col-6">
-//             <div className="input-group">
-//               <div className="input-group-prepend">
-//                 <span className="input-group-text">₹</span>
-//               </div>
-//               <input
-//                 type="number"
-//                 className="form-control"
-//                 placeholder="Price"
-//                 value={price}
-//                 onChange={(event) => setPrice(event.target.value)}
-//               ></input>
-//             </div>
-//           </div>
-//           <div className="col-6">
-//             <div className="input-group">
-//               <div className="input-group-prepend">
-//                 <span className="input-group-text">₹</span>
-//               </div>
-//               <input
-//                 type="number"
-//                 className="form-control"
-//                 placeholder="Ratings"
-//                 value={rating}
-//                 onChange={(event) => setRating(event.target.value)}
-//               ></input>
-//             </div>
-//           </div>
-//           <div className="col-8 my-3">
-//             <div className="input-group">
-//               <div className="input-group-prepend">
-//                 <span className="input-group-text">₹</span>
-//               </div>
-//               <input
-//                 type="number"
-//                 className="form-control"
-//                 placeholder="Quantity"
-//                 value={quantity}
-//                 onChange={(event) => setQuantity(event.target.value)}
-//               ></input>
-//             </div>
-//           </div>
-//           <div className="col-4 my-3">
-//             <select
-//               className="custom-select"
-//               id="inputGroupSelect02"
-//               value={quantityType}
-//               onChange={(event) => setQuantityType(event.target.value)}
-//             >
-//               <option defaultValue>Units</option>
-//               <option value="1">Kilogram</option>
-//               <option value="2">Gram</option>
-//               <option value="3">Dozen</option>
-//             </select>
-//           </div>
-//           <div className="col-12 mb-3">
-//             <input
-//               type="submit"
-//               className="btn btn-primary btn-block"
-//               name="Submit"
-//             />
-//           </div>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default InputForm;
