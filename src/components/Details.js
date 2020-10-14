1327d162ec24188b4d1232084e8d07c47141687d
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-15 03:04:02 +0530

    Fetched Particular Item

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,28 +1,40 @@
 import React from "react";
 import PriceCardLayout from "./PriceCardLayout";
-import InputForm from './InputForm';
+import InputForm from "./InputForm";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Details = () => {
-  const imgLink =
-    "https://images.unsplash.com/photo-1474564862106-1f23d10b9d72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1368&q=80";
+  let { id } = useParams();
+
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/items/` + id)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setItem(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  });
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 col-lg-4">
           <img
-            src={imgLink}
-            alt="Apple"
+            src={item.mainImgUrl}
+            alt="Item"
             className="img-fluid rounded shadow-sm"
           />
-          <h2 className="mt-4 font-weight-bold">Apples</h2>
+          <h2 className="mt-4 font-weight-bold">{item.name}</h2>
           <h5 className="my-2">Price - $10 per KG</h5>
-          <p className="my-4 text-black-50">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-            cupiditate a modi, adipisci ex totam, necessitatibus ullam voluptate
-            exercitationem sapiente eligendi iure debitis hic alias aliquam
-            voluptatibus voluptatum recusandae earum?
-          </p>
+          <p className="my-4 text-black-50">{item.description}</p>
           <InputForm />
         </div>
         <div className="col-12 col-lg-8">
