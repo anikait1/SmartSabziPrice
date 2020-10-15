8c0ddcc221fc72e8d674e499db63379d8c03a37a
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-15 12:49:16 +0530

    Fetched Item Details

--- src/components/Item/ItemDetail.jsx
+++ src/components/Item/ItemDetail.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import PricePostLayout from "../PricePost/PricePostLayout";
-import PricePostInput from '../PricePost/PricePostInput';
+import PricePostInput from "../PricePost/PricePostInput";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Details = () => {
-  const imgLink =
-    "https://images.unsplash.com/photo-1474564862106-1f23d10b9d72?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1368&q=80";
+  const { id } = useParams();
+  const [item, setItem] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/items/` + id)
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setItem(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  });
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-12 col-lg-4">
           <img
-            src={imgLink}
+            src={item.mainImgUrl}
             alt="Apple"
             className="img-fluid rounded shadow-sm"
           />
-          <h2 className="mt-4 font-weight-bold">Apples</h2>
+          <h2 className="mt-4 font-weight-bold">{item.name}</h2>
           <h5 className="my-2">Price - $10 per KG</h5>
-          <p className="my-4 text-black-50">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-            cupiditate a modi, adipisci ex totam, necessitatibus ullam voluptate
-            exercitationem sapiente eligendi iure debitis hic alias aliquam
-            voluptatibus voluptatum recusandae earum?
-          </p>
+          <p className="my-4 text-black-50">{item.description}</p>
           <PricePostInput />
         </div>
         <div className="col-12 col-lg-8">
