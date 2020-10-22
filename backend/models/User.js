0a4bdb6aab9b3571e00f468f036ac655e4ebece2
Sahil Rajput <sahilrajput85579@gmail.com>
2020-10-22 19:59:17 +0530

    Updated User's Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,74 @@
 import mongoose from "mongoose";
+import crypto from "crypto";
 
-const UserSchema = new mongoose.Schema({
-  // user info
-  username: { type: String, required: true, trim: true },
-  password: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
-});
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    hashed_password: {
+      type: String,
+      required: true,
+    },
+    salt: String,
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    resetPasswordLink: {
+      data: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export default mongoose.model("User", UserSchema);
+// virtual
+// Not stored in MongoDb
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+// methods
+userSchema.methods = {
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function (password) {
+    if (!password) return "";
+    try {
+      return crypto
+      // Creates a Hmac(Hashed based message authentication code) object using the specified algorithm and key
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
+export default mongoose.model("User", userSchema);
