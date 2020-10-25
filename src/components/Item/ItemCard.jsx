c1e727b7aa89cf93052c14d360c3d53e5d1180eb
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-25 21:29:18 +0530

    Update ItemCard.jsx
    Updated the card design to have find price button

--- src/components/Item/ItemCard.jsx
+++ src/components/Item/ItemCard.jsx
@@ -4,21 +4,15 @@ import "../../index.css";
 
 const ItemCard = ({ id, name, calorieCount, imgLink }) => {
   return (
-    <Link to={"/details/" + id}>
-      <div className="card border-white shadow-sm bottom-rounded-border top-rounded-border">
-        <img
-          src={imgLink}
-          alt={name}
-          className="card-img-top top-rounded-border"
-        />
-        <div className="card-body text-center p-2">
-          <h6 className="card-title font-weight-bold my-1">{name}</h6>
-          <div className="card-text text-black-50 small">
-            Calories - {calorieCount}
-          </div>
-        </div>
+    <div className="card shadow-sm">
+      <img src={imgLink} alt={name} className="card-img-top" />
+      <div className="card-body text-center p-2">
+        <h6 className="card-title font-weight-bold my-2">{name}</h6>
+        <Link to={"details/" + id} className="btn btn-info btn-sm btn-block">
+          Find Price
+        </Link>
       </div>
-    </Link>
+    </div>
   );
 };
 
