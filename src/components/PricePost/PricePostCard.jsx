8dee28dd62c46e295433d5540c8d88ef58701a83
Anikait <50035624+anikait1@users.noreply.github.com>
2020-10-25 21:30:59 +0530

    Update PricePostCard.jsx
    Replaced bootstrap card with material-ui card

--- src/components/PricePost/PricePostCard.jsx
+++ src/components/PricePost/PricePostCard.jsx
@@ -1,83 +1,97 @@
 import React from "react";
 import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
+import { red } from "@material-ui/core/colors";
+import CardMedia from "@material-ui/core/CardMedia";
+import Divider from "@material-ui/core/Divider";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import LocalMallOutlinedIcon from "@material-ui/icons/LocalMallOutlined";
+import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
+import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@material-ui/icons/FavoriteOutlined";
+import IconButton from "@material-ui/core/IconButton";
 
-// Helper Components
-const SplitRow = ({ iconOne, iconOneText, iconTwo, iconTwoText }) => {
+const useStyles = makeStyles((theme) => ({
+  avatar: {
+    backgroundColor: red[500],
+  },
+  likes: {
+    marginLeft: "auto",
+  },
+}));
+
+const MaterialPriceCard = ({ user, itemBill, location, time }) => {
+  const classes = useStyles();
+  const [clicked, setClicked] = useState(false);
+
   return (
-    <div className="row no-gutters align-items-center">
-      <div className="col-6">
-        <i className={`fas fa-${iconOne} mx-2`}></i>
-        <span className="small font-weight-bold">{iconOneText}</span>
-      </div>
-      <div className="col-6 my-1">
-        <i className={`fas fa-${iconTwo} mx-2`}></i>
-        <span className="small font-weight-bold">{iconTwoText}</span>
-      </div>
-    </div>
+    <Card>
+      <CardMedia
+        className={classes.media}
+        component="img"
+        src="https://blog.pixlr.com/wp-content/uploads/2019/03/stars-pattern.png"
+        title="Paella dish"
+        height={100}
+      />
+      <CardHeader
+        avatar={
+          <Avatar aria-label="price post" className={classes.avatar}>
+            {user.username[0].toUpperCase()}
+          </Avatar>
+        }
+        title={user.username}
+        subheader={time.split("T")[0]}
+      />
+      <Divider variant="middle" />
+      <CardContent style={{paddingTop: 8, paddingBottom: 8}}>
+        <List dense={true}>
+          <ListItem disableGutters>
+            <ListItemIcon style={{ minWidth: 40 }}>
+              <ExploreOutlinedIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>
+              {`${location.pincode}, ${location.city}`}
+            </ListItemText>
+          </ListItem>
+          <ListItem disableGutters>
+            <ListItemIcon style={{ minWidth: 40 }}>
+              <LocalMallOutlinedIcon fontSize="small" />
+            </ListItemIcon>
+            <ListItemText>
+              {`${itemBill.quantity} ${itemBill.quantityType}`}
+            </ListItemText>
+          </ListItem>
+        </List>
+      </CardContent>
+      <Divider variant="middle" />
+      <CardActions disableSpacing style={{ paddingLeft: 16, paddingRight: 16 }}>
+        <Typography variant="h5" component="h5">
+          &#8377; {itemBill.price}
+        </Typography>
+        <IconButton
+          aria-label="like the price"
+          className={classes.likes}
+          onClick={() => {
+            clicked ? setClicked(false) : setClicked(true);
+          }}
+        >
+          {clicked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+        </IconButton>
+        <Typography variant="subtitile1" component="h6" color="textSecondary">
+          255
+        </Typography>
+      </CardActions>
+    </Card>
   );
 };
 
-const InfoColumn = ({ icon, iconText, bottomText, color }) => {
-  return (
-    <div className="col-2 text-center">
-      <i className={`fas fa-${icon} fa-xs text-${color}`}></i>
-      <h6 className={`my-1 font-weight-bold text-${color}`}>{iconText}</h6>
-      <span className={`small font-weight-bold text-${color}`}>{bottomText}</span>
-    </div>
-  );
-};
-
-// Main Component
-const PriceCard = ({ user, itemBill, location, time }) => {
-  const [voteCount, setVoteCount] = useState(0);
-  return (
-    <div className="card shadow-sm rounded rounded-lg border-0">
-      <div className="row no-gutters align-items-center">
-        <div className="col-4">
-          <img src={user.imgLink} alt="User" className="img-fluid" />
-        </div>
-        <InfoColumn
-          icon="money-bill-alt"
-          iconText={itemBill.price}
-          bottomText="$"
-          color="success"
-        />
-        <InfoColumn
-          icon="shopping-bag"
-          iconText={itemBill.quantity}
-          bottomText={itemBill.quantityType}
-          color="primary"
-        />
-        <InfoColumn
-          icon="star"
-          iconText={itemBill.rating}
-          bottomText="Stars"
-          color="warning"
-        />
-        <div className="col-2 text-center">
-          <i className="fas fa-chevron-up fa-lg d-block text-success" onClick={() => setVoteCount(voteCount + 1)}></i>
-          <p className="my-0 font-weight-bold text-success my-2">{voteCount}</p>
-          <i className="fas fa-chevron-down fa-lg d-block text-danger" onClick={() => setVoteCount(voteCount - 1)}></i>
-        </div>
-
-        <div className="col-12 bg-light py-2">
-          <SplitRow
-            iconOne="calendar-minus"
-            iconOneText={time.split("T")[0]}
-            iconTwo="compass"
-            iconTwoText={`${location.pincode}, ${location.city}`}
-          />
-
-          <SplitRow
-            iconOne="user"
-            iconOneText={user.username}
-            iconTwo="clock"
-            iconTwoText={time.split("T")[1]}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default PriceCard;
+export default MaterialPriceCard;
