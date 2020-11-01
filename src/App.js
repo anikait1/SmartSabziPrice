5cd3bd819aa001898affbdd94877df0a3075a06c
Sahil Rajput <sahilrajput85579@gmail.com>
2020-11-02 00:59:35 +0530

    Added more routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import HomePage from "./components/HomePage";
 import Details from "./components/Item/ItemDetail";
 import Profile from "./components/Profile";
 import Navbar from "./components/Navbar";
+import Register from "./components/AuthScreens/Register";
+import Activate from "./components/AuthScreens/Activate";
+import Login from "./components/AuthScreens/Login";
+import ForgotPassword from "./components/AuthScreens/ForgotPassword";
+import Reset from "./components/AuthScreens/Reset";
 
 function App() {
   return (
@@ -13,6 +18,11 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/details/:id" component={Details} />
         <Route path="/profile" component={Profile} />
+        <Route path="/register" component = {Register} />
+        <Route path="/users/activate/:token" component = {Activate} />
+        <Route path="/login" component = {Login} />
+        <Route path='/users/password/forget' component = {ForgotPassword} />
+        <Route path='/users/password/reset/:token' component = {Reset} />
       </Switch>
     </React.Fragment>
   );
