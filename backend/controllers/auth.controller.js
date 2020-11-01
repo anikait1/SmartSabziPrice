a6e69323b776e758cde44466035f05cf58c89299
Sahil Rajput <sahilrajput85579@gmail.com>
2020-11-02 00:57:07 +0530

    Updated Auth Controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,13 +6,11 @@ import expressJwt from "express-jwt";
 // contains tools to simplify programming with strings, numbers, arrays, functions
 import _ from "lodash";
 
-//TO authenticate with google services
-const { OAuth2Client } = 'google-auth-library';
-import fetch from "node-fetch";
-
 //For enabling require keyword
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
+
+import fetch from "node-fetch";
 
 //For server side data validation and sanitization
 const { validationResult } = require('express-validator');
@@ -65,10 +63,10 @@ export function registerController(req, res) {
     //Email Data
     const emailData = {
       from: process.env.EMAIL_FROM,
-      to: process.env.EMAIL_TO,
+      to: email,
       subject: 'Account activation link',
       html: `
-                <h1>Please Click the following Link to activate your account</h1>
+                <h1>Please Click on the following Link to activate your account</h1>
                 <p>${process.env.CLIENT_URL}/users/activate/${token}</p>
                 <hr />
                 <p>This email may containe sensitive information</p>
@@ -92,3 +90,232 @@ export function registerController(req, res) {
   }
 
 }
+
+export function activationController(req, res){
+  const { token } = req.body;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_ACCOUNT_ACTIVATION, (err, decoded) => {
+      if (err) {
+        console.log('Activation error');
+        return res.status(401).json({
+          errors: 'Expired link. Signup again'
+        });
+      } else {
+        const { name, email, password } = jwt.decode(token);
+
+        console.log(email);
+        const user = new User({
+          name,
+          email,
+          password
+        });
+
+        user.save((err, user) => {
+          if (err) {
+            console.log('Save error', errorHandler(err));
+            return res.status(401).json({
+              errors: errorHandler(err)
+            });
+          } else {
+            return res.json({
+              success: true,
+              message: user,
+              message: 'Signup success'
+            });
+          }
+        });
+      }
+    });
+  } else {
+    return res.json({
+      message: 'Error Please try again'
+    });
+  }
+};
+
+export function loginController(req, res){
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map(error => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError
+    });
+  } else {
+    // check if user exist
+    User.findOne({
+      email
+    }).exec((err, user) => {
+      if (err || !user) {
+        return res.status(400).json({
+          errors: 'User with that email does not exist. Please signup'
+        });
+      }
+      // authenticate
+      if (!user.authenticate(password)) {
+        return res.status(400).json({
+          errors: 'Email and password do not match'
+        });
+      }
+      // generate a token and send to client
+      const token = jwt.sign(
+        {
+          _id: user._id
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: '30d'
+        }
+      );
+      const { _id, name, email, role } = user;
+
+      return res.json({
+        token,
+        user: {
+          _id,
+          name,
+          email,
+          role
+        }
+      });
+    });
+  }
+};
+
+export function forgotPasswordController(req, res){
+  const { email } = req.body;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map(error => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError
+    });
+  } else {
+    User.findOne(
+      {
+        email
+      },
+      (err, user) => {
+        if (err || !user) {
+          return res.status(400).json({
+            error: 'User with that email does not exist'
+          });
+        }
+
+        const token = jwt.sign(
+          {
+            _id: user._id
+          },
+          process.env.JWT_RESET_PASSWORD,
+          {
+            expiresIn: '10m'
+          }
+        );
+
+        const emailData = {
+          from: process.env.EMAIL_FROM,
+          to: email,
+          subject: `Password Reset link`,
+          html: `
+                    <h1>Please use the following link to reset your password</h1>
+                    <p>${process.env.CLIENT_URL}/users/password/reset/${token}</p>
+                    <hr />
+                    <p>This email may contain sensetive information</p>
+                    <p>${process.env.CLIENT_URL}</p>
+                `
+        };
+
+        return user.updateOne(
+          {
+            resetPasswordLink: token
+          },
+          (err, success) => {
+            if (err) {
+              console.log('RESET PASSWORD LINK ERROR', err);
+              return res.status(400).json({
+                error:
+                  'Database connection error on user password forgot request'
+              });
+            } else {
+              sgMail
+                .send(emailData)
+                .then(sent => {
+                  // console.log('SIGNUP EMAIL SENT', sent)
+                  return res.json({
+                    message: `Email has been sent to ${email}. Follow the instruction to activate your account`
+                  });
+                })
+                .catch(err => {
+                  // console.log('SIGNUP EMAIL SENT ERROR', err)
+                  return res.json({
+                    message: err.message
+                  });
+                });
+            }
+          }
+        );
+      }
+    );
+  }
+};
+
+export function resetPasswordController(req, res){
+  const { resetPasswordLink, newPassword } = req.body;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array().map(error => error.msg)[0];
+    return res.status(422).json({
+      errors: firstError
+    });
+  } else {
+    if (resetPasswordLink) {
+      jwt.verify(resetPasswordLink, process.env.JWT_RESET_PASSWORD, function(
+        err,
+        decoded
+      ) {
+        if (err) {
+          return res.status(400).json({
+            error: 'Expired link. Try again'
+          });
+        }
+
+        User.findOne(
+          {
+            resetPasswordLink
+          },
+          (err, user) => {
+            if (err || !user) {
+              return res.status(400).json({
+                error: 'Something went wrong. Try later'
+              });
+            }
+
+            const updatedFields = {
+              password: newPassword,
+              resetPasswordLink: ''
+            };
+
+            user = _.extend(user, updatedFields);
+
+            user.save((err, result) => {
+              if (err) {
+                return res.status(400).json({
+                  error: 'Error resetting user password'
+                });
+              }
+              res.json({
+                message: `Great! Now you can login with your new password`
+              });
+            });
+          }
+        );
+      });
+    }
+  }
+};
+
+
