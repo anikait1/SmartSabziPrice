6a61feea8392d3a358e42f8139aac4dbe8846afd
Sahil Rajput <sahilrajput85579@gmail.com>
2020-11-02 01:01:16 +0530

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import InputBase from "@material-ui/core/InputBase";
+import { fade } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import { signout, isAuth } from "../helpers/auth";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    marginBottom: theme.spacing(6)
+    marginBottom: theme.spacing(6),
+  },
+  item : {
+    marginRight : theme.spacing(35),
+  },
+  right : {
+      marginRight : theme.spacing(2),
+      marginLeft : theme.spacing(2),
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -18,22 +33,156 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+    marginRight : theme.spacing(4),
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
 }));
 
 export default function Navbar() {
   const classes = useStyles();
+  const history = useHistory();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleProfile = () => {
+    setAnchorEl(null);
+    history.push("/profile");
+  };
+
+  const handleLogout = () => {
+    signout();
+    setAnchorEl(null);
+    history.push("/");
+  };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Foxy Buyer
+          <Typography
+            variant="h6"
+            className={classes.title}
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            FoxyBuyer
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" classes={{root: classes.item}}>Vegetables</Button>
+          <Button color="inherit" classes={{root: classes.item}}>Fruits</Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search Item"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          {isAuth() ? (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+              >
+                <MenuItem onClick={handleProfile}>My Profile</MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <div>
+              <Button
+                color="inherit"
+                classes={{root: classes.right}}
+                onClick={() => {
+                  history.push("/login");
+                }}
+              >
+                Login
+              </Button>
+              <Button
+                color="inherit"
+                classes={{root: classes.right}}
+                onClick={() => {
+                  history.push("/register");
+                }}
+              >
+                Sign Up
+              </Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
