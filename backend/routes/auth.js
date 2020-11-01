a305a54c50a94ce5f3b2e948f0030c890a073e53
Sahil Rajput <sahilrajput85579@gmail.com>
2020-11-02 00:58:19 +0530

    Updated Auth Route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,28 @@
 import express from "express";
 const router = express.Router();
 
-//Load Controllers
-import {registerController} from '../controllers/auth.controller.js';
+//Validations
+import {
+    validSign,
+    validLogin,
+    forgotPasswordValidator,
+    resetPasswordValidator,
+} from '../helpers/valid.js';
 
-router.post('/register', registerController);
+//Load Controllers
+import {
+    registerController,
+    activationController,
+    loginController,
+    forgotPasswordController,
+    resetPasswordController,
+} from '../controllers/auth.controller.js';
+
+router.post('/register', validSign,registerController);
+router.post('/activation', activationController);
+router.post('/login',validLogin,loginController);
+
+router.put('/forgotpassword',forgotPasswordValidator,forgotPasswordController);
+router.put('/resetpassword',resetPasswordValidator,resetPasswordController);
 
 export default router;
