464710af658397f84f083c8fdbad8d511bf4db3e
Sahil Rajput <sahilrajput85579@gmail.com>
2020-11-03 14:30:16 +0530

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,106 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
 import InputBase from "@material-ui/core/InputBase";
 import { fade } from "@material-ui/core/styles";
 import SearchIcon from "@material-ui/icons/Search";
 import { signout, isAuth } from "../helpers/auth";
 import { useHistory } from "react-router-dom";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+
+const drawerWidth = 195;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
-    marginBottom: theme.spacing(6),
+    display: "flex",
   },
-  item : {
-    marginRight : theme.spacing(35),
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
-  right : {
-      marginRight : theme.spacing(2),
-      marginLeft : theme.spacing(2),
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+  item: {
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
   },
   title: {
     flexGrow: 1,
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+  right: {
+    marginRight: theme.spacing(4),
+    marginLeft: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
+    },
   },
   search: {
     position: "relative",
@@ -40,11 +109,14 @@ const useStyles = makeStyles((theme) => ({
     "&:hover": {
       backgroundColor: fade(theme.palette.common.white, 0.25),
     },
-    marginLeft: 0,
+    marginLeft: theme.spacing(40),
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
       width: "auto",
+    },
+    [theme.breakpoints.down("sm")]: {
+      display: "none",
     },
   },
   searchIcon: {
@@ -58,7 +130,7 @@ const useStyles = makeStyles((theme) => ({
   },
   inputRoot: {
     color: "inherit",
-    marginRight : theme.spacing(4),
+    marginRight: theme.spacing(4),
   },
   inputInput: {
     padding: theme.spacing(1, 1, 1, 0),
@@ -78,11 +150,17 @@ const useStyles = makeStyles((theme) => ({
 export default function Navbar() {
   const classes = useStyles();
   const history = useHistory();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
+  const anchorOpen = Boolean(anchorEl);
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
   const handleProfile = () => {
@@ -96,15 +174,30 @@ export default function Navbar() {
     history.push("/");
   };
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
         <Toolbar>
           <IconButton
-            edge="start"
-            className={classes.menuButton}
             color="inherit"
-            aria-label="menu"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
           >
             <MenuIcon />
           </IconButton>
@@ -117,8 +210,14 @@ export default function Navbar() {
           >
             FoxyBuyer
           </Typography>
-          <Button color="inherit" classes={{root: classes.item}}>Vegetables</Button>
-          <Button color="inherit" classes={{root: classes.item}}>Fruits</Button>
+          <div>
+            <Button color="inherit" classes={{ root: classes.item }}>
+              Vegetables
+            </Button>
+            <Button color="inherit" classes={{ root: classes.item }}>
+              Fruits
+            </Button>
+          </div>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
               <SearchIcon />
@@ -155,7 +254,8 @@ export default function Navbar() {
                   vertical: "top",
                   horizontal: "right",
                 }}
-                open={open}
+                open={anchorOpen}
+                onClose={handleClose}
               >
                 <MenuItem onClick={handleProfile}>My Profile</MenuItem>
                 <MenuItem onClick={handleLogout}>Logout</MenuItem>
@@ -165,26 +265,67 @@ export default function Navbar() {
             <div>
               <Button
                 color="inherit"
-                classes={{root: classes.right}}
+                classes={{ root: classes.right }}
                 onClick={() => {
                   history.push("/login");
                 }}
               >
                 Login
               </Button>
-              <Button
-                color="inherit"
-                classes={{root: classes.right}}
-                onClick={() => {
-                  history.push("/register");
-                }}
-              >
-                Sign Up
-              </Button>
             </div>
           )}
         </Toolbar>
       </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="temporary"
+        onEscapeKeyDown={handleDrawerClose}
+        onBackdropClick={handleDrawerClose}
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem>
+            <Button>Vegetables</Button>
+          </ListItem>
+          <ListItem>
+            <Button>Fruits</Button>
+          </ListItem>
+          {isAuth() ? null : (
+            <ListItem>
+              <Button
+                onClick={() => {
+                  history.push("/login");
+                  handleDrawerClose();
+                }}
+              >
+                Login
+              </Button>
+            </ListItem>
+          )}
+        </List>
+      </Drawer>
+
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+      </main>
     </div>
   );
 }
