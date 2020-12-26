ed366e5c8dae3eb82cafcb076e8760579af4c748
Anikait <anikait.makkar@outlook.com>
2020-12-26 18:12:44 +0530

    Added index startup file

---
+++ backend-v2/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const itemRouter = require("./resources/item/item.route");
+const userRouter = require("./resources/user/user.router");
+
+// set up express
+const app = express();
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dbAdmin:lkYXgjB4kemszZU2@sabzicluster.knl8u.mongodb.net/DummyFox?retryWrites=true&w=majority";
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("error", err);
+});
+
+mongoose.connection.once("open", () => {
+  console.log("Mongo is done");
+});
+
+app.use("/items", itemRouter);
+app.use("/users", userRouter);
+
+app.listen(5000, () => {
+  console.log("Server is up");
+});
