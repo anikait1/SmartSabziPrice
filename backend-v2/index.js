1d438a0846857dd367b37fbd0f4a517a65c4a889
Anikait <anikait.makkar@outlook.com>
2021-01-05 15:05:07 +0530

    Added cors for frontend app

--- backend-v2/index.js
+++ backend-v2/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const itemRouter = require("./resources/item/item.route");
 const userRouter = require("./resources/user/user.router");
@@ -7,6 +8,7 @@ const userRouter = require("./resources/user/user.router");
 // set up express
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 const uri =
   "mongodb+srv://dbAdmin:lkYXgjB4kemszZU2@sabzicluster.knl8u.mongodb.net/DummyFox?retryWrites=true&w=majority";
