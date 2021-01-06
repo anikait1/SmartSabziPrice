46c879cfa3c6fa9d0cbf42bb69589bef5e6b19ec
Sahil Rajput <sahilrajput85579@gmail.com>
2021-01-06 22:52:24 +0530

    Upated App component

--- frontend-v2/src/App.js
+++ frontend-v2/src/App.js
@@ -1,29 +1,35 @@
-// import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import ItemDetail from "./components/ItemDetail";
 import PricePostDetail from "./components/PricePostDetail"
 import Profile from "./components/Profile";
-
+import Register from "./components/AuthScreens/Register";
+import Login from "./components/AuthScreens/Login";
+import ForgotPassword from "./components/AuthScreens/ForgotPassword";
+import Reset from "./components/AuthScreens/Reset";
+import Activate from "./components/AuthScreens/Activate";
 
 function App() {
   return (
-    <div>
-      <Header/>
-      <PricePostDetail/>
-    </div>
-    // <React.Fragment>
-    //   <Switch>
-    //     <Route exact path="/" component={HomePage} />
-    //     <Route path="/details/:id" component={Details} />
-    //     <Route path="/profile" component={Profile} />
-    //     <Route path="/register" component={Register} />
-    //     <Route path="/users/activate/:token" component={Activate} />
-    //     <Route path="/login" component={Login} />
-    //     <Route path="/users/password/forget" component={ForgotPassword} />
-    //     <Route path="/users/password/reset/:token" component={Reset} />
-    //   </Switch>
-    // </React.Fragment>
+    // <div>
+    //   <Header/>
+    //   <PricePostDetail/>
+    // </div>
+    <React.Fragment>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/details/:id" component={ItemDetail} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/register" component={Register} />
+        <Route path="/users/activate/:token" component={Activate} />
+        <Route path="/login" component={Login} />
+        <Route path="/users/password/forget" component={ForgotPassword} />
+        <Route path="/users/password/reset/:token" component={Reset} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
