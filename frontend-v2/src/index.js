8c740f79726a3df5c25b400e995f22013b584b6a
Sahil Rajput <sahilrajput85579@gmail.com>
2021-01-06 22:54:16 +0530

    Updated index.js

--- frontend-v2/src/index.js
+++ frontend-v2/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-
+import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
