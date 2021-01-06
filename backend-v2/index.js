2208503785872a7df0586513c47835f2942fb0d8
Sahil Rajput <sahilrajput85579@gmail.com>
2021-01-06 22:49:33 +0530

    Updated index.js

--- backend-v2/index.js
+++ backend-v2/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const morgan = require('morgan');
+require("dotenv").config();
 
 const itemRouter = require("./resources/item/item.route");
 const userRouter = require("./resources/user/user.route");
+const authRouter = require("./resources/auth/auth.route");
 
 // set up express
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(morgan("dev"));
 
 const uri =
   "mongodb+srv://dbAdmin:lkYXgjB4kemszZU2@sabzicluster.knl8u.mongodb.net/DummyFox?retryWrites=true&w=majority";
@@ -28,6 +32,8 @@ mongoose.connection.once("open", () => {
 
 app.use("/items", itemRouter);
 app.use("/users", userRouter);
+app.use("/api/", authRouter);
+
 
 app.listen(5000, () => {
   console.log("Server is up");
